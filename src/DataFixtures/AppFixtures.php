0e9d80597e42952adea3cf2d011dffa3f93d6593
peter <codepit84@gmail.com>
2023-04-27 09:32:45 +0200

    Commit

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,6 +5,7 @@ namespace App\DataFixtures;
 use App\Entity\Author;
 use App\Entity\Book;
 use App\Entity\Country;
+use App\Entity\Gender;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker;
@@ -27,6 +28,44 @@ class AppFixtures extends Fixture
             $manager->persist($country);
         }
 
+        // Les Genres
+        $gender1 = new Gender();
+        $gender1->setName('Autobiographie');
+        $manager->persist($gender1);
+
+        $gender2 = new Gender();
+        $gender2->setName('Biographie');
+        $manager->persist($gender2);
+
+        $gender3 = new Gender();
+        $gender3->setName('Conte');
+        $manager->persist($gender3);
+
+        $gender4 = new Gender();
+        $gender4->setName('Epistolaire');
+        $manager->persist($gender4);
+
+        $gender5 = new Gender();
+        $gender5->setName('Essai');
+        $manager->persist($gender5);
+
+        $gender6 = new Gender();
+        $gender6->setName('Fable');
+        $manager->persist($gender6);
+
+        $gender7 = new Gender();
+        $gender7->setName('Nouvelle');
+        $manager->persist($gender7);
+
+        $gender8 = new Gender();
+        $gender8->setName('Pamphlet');
+        $manager->persist($gender8);
+
+        $gender9 = new Gender();
+        $gender9->setName('Roman');
+        $manager->persist($gender9);
+        
+
         // Les Auteurs
         $authors= [];
 
@@ -48,8 +87,9 @@ class AppFixtures extends Fixture
 
         for ($i=1; $i <=25 ; $i++) { 
             $book = new Book();
-            $book->setTitle($faker->sentence($nbWords = 4, $variableNbWords = true));
-            $book->setType('RP654ENJ');
+            // $book->setTitle($faker->sentence($nbWords = 4, $variableNbWords = true));
+            $book->setTitle($faker->words(4, true));
+            $book->setType('Type');
             $book->addAuthor($authors[mt_rand(0, 24)]);
 
             $manager->persist($book);
--- src/Form/GenderFormType.php
+++ src/Form/GenderFormType.php
@@ -6,13 +6,16 @@ use App\Entity\Gender;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class GenderFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
+            ->add('name', TextType::class, [
+                'label' => 'Nom du genre',
+            ])
         ;
     }
 
